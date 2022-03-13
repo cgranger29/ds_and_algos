6d3b971c64b7ad2571ee71d4e266dd07579249a2
cgranger29 <cgranger29@gmail.com>
2022-03-13 15:31:02 -0400

    added shiftr, add, and getDecimal methods

--- binary_number/src/BinaryHandler.java
+++ binary_number/src/BinaryHandler.java
@@ -1,22 +1,24 @@
 public class BinaryHandler{
 
     public static void main(String[] args){
-        BinaryNumber test = new BinaryNumber(9);
+        BinaryNumber test = new BinaryNumber("100101000000000000000000");
         // test.getBinaryNumber();
 
         // test.printLength();
-        BinaryNumber test2 = new BinaryNumber("10010001010100010010100");
+        BinaryNumber test2 = new BinaryNumber("1001");
         // test2.getBinaryNumber();
 
         // test2.printLength();
 
         // System.out.println("getDigit return value: " + test2.getDigit(2));
 
-        test2.shiftR(1000);
-
         test2.getBinaryNumber();
 
-        System.out.println(test2.toString());
+        test2.add(test);
+
+        System.out.println("New number after add: " + test2.toString());
+
+        System.out.println("New number to decimal: " + test2.toDecimal());
 
 
     }
--- binary_number/src/BinaryNumber.java
+++ binary_number/src/BinaryNumber.java
@@ -34,7 +34,68 @@ public class BinaryNumber{
     }
 
     public void add(BinaryNumber aBinaryNumber){
-        // need to implement
+        // 0 + 0 = 0, 0 carryover
+        // 0 + 1 ( or 1 + 0) = 1, 0 carryover
+        // 1 + 1 = 0, 1 carryover
+        // 1 + 1 + 1 (this implies a 1 + 1 but with 1 carryover which is max) = 1, 1 carryover
+        
+        // to make this easier the binary number with smallest len should be padded to match
+        // if lens are the same no need for this
+        if(data.length == aBinaryNumber.getLength()){
+            // add them together with rules
+            System.out.println("Lengths are equal");
+        } else if(aBinaryNumber.getLength() < data.length){
+            aBinaryNumber.shiftR((data.length - aBinaryNumber.getLength()));
+
+            System.out.println("aBinaryNumber padded and is now: " + aBinaryNumber.toString());
+        } else{
+            shiftR((aBinaryNumber.getLength() - this.getLength()));
+            System.out.println("Current object padded and is now: " + this.toString());
+        }
+
+        //now the add logic, need to loop from end to start and keep track of carryover
+        boolean carryOver = false;
+        int[] newNum = new int[this.getLength()];
+
+        System.out.println("Adding: " + this.toString() + " and " + aBinaryNumber.toString());
+        //new array to hold new binary number
+        for (int i = 0; i < this.getLength(); i++){
+            newNum[i] = 0;
+        }
+        
+        for(int i = this.getLength() - 1; i >= 0; i--){
+            if(this.getDigit(i) == 0 && aBinaryNumber.getDigit(i) == 0 && !carryOver){
+                newNum[i] = 0;
+
+            } else if(this.getDigit(i) == 1 && aBinaryNumber.getDigit(i) == 0 && !carryOver){
+                newNum[i] = 1;
+            } else if (this.getDigit(i) == 0 && aBinaryNumber.getDigit(i) == 1 && !carryOver){
+                newNum[i] = 1;
+            } else if (this.getDigit(i) == 1 && aBinaryNumber.getDigit(i) == 1 && !carryOver){
+                newNum[i] = 0;
+                carryOver = true;
+            } else if(this.getDigit(i) == 0 && aBinaryNumber.getDigit(i) == 0 && carryOver){
+                newNum[i] = 1;
+                carryOver = false;
+            } else if (this.getDigit(i) == 1 && aBinaryNumber.getDigit(i) == 0 && carryOver){
+                newNum[i] = 0;
+                carryOver = true;
+            } else if (this.getDigit(i) == 0 && aBinaryNumber.getDigit(i) == 1 && carryOver){
+                newNum[i] = 0;
+                carryOver = true;
+            } else if(this.getDigit(i) == 1 && aBinaryNumber.getDigit(i) == 1 && carryOver){
+                newNum[i] = 1;
+                carryOver = true;
+
+            }
+        }
+
+        //need one last check in the above that states if its the last iteration of the loop and we get a 1 + 1 + 1 (carryOver is true)
+        //then we need to add one to the start of the binary number (so if above true and num is 1100 we make it 11100)
+
+        data = newNum;
+        
+      
     }
 
     @Override
@@ -42,9 +103,20 @@ public class BinaryNumber{
         return Arrays.toString(data);
     }
 
-    // public int toDecimal(){
-    //     // need to implement
-    // }
+    public int toDecimal(){
+        // loop the current data array and keep a cumulative sum of the digits. 2^0 + 2^1 etc...
+        int decimalSum = 0;
+        int exp = this.getLength() - 1;
+        for(int i = 0; i < this.getLength(); i++){
+            if(this.getDigit(i) == 1){
+                decimalSum += Math.pow(2, exp);
+            }
+
+            exp -= 1;
+        }
+
+        return decimalSum;
+    }
 
     public void clearOverflow(){
         // need to implement
