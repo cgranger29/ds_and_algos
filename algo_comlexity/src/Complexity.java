dfe80ce663250db0c8397be5dc6b92922e484f86
cgranger29 <cgranger29@gmail.com>
2022-03-26 16:18:36 -0400

    adding algo_complexity

---
+++ algo_comlexity/src/Complexity.java
@@ -0,0 +1,25 @@
+public class Complexity {
+
+    public static void method1(){
+        // n^2
+    }
+
+    public static void method2(){
+        // n^3
+    }
+
+    public static void method3(){
+        // logn
+    }
+
+    public static void method4(){
+        // nlogn
+    }
+
+    public static void method5(){
+        // loglogn
+    }
+    public static void main(String args[]){
+        
+    }
+}
